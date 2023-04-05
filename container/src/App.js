077309fc5d6549d0cdf6ce6930035da40e7f97ed
manideepb07 <manideep.b07@gmail.com>
2023-04-05 22:37:01 +0530

    Initial Commit

---
+++ container/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MarketingAppComponent from "./components/marketingapp.component";
+
+export default () => {
+  return (
+    <>
+      <h1>Hi There!</h1>
+      <hr />
+      <MarketingAppComponent />
+    </>
+  );
+};
