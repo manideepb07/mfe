e3d3411fb102d114527b92ff40621157fac43504
manideepb07 <manideep.b07@gmail.com>
2023-04-06 17:19:10 +0530

    rndomise prod classnames

--- container/src/App.js
+++ container/src/App.js
@@ -1,12 +1,27 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import MarketingAppComponent from "./components/marketingapp.component";
+import Header from "./components/header.component";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 export default () => {
   return (
     <>
-      <h1>Hi There Test!</h1>
-      <hr />
-      <MarketingAppComponent />
+      <StylesProvider generateClassName={generateClassName}>
+        <BrowserRouter>
+          <div>
+            <Header />
+            <MarketingAppComponent />
+          </div>
+        </BrowserRouter>
+      </StylesProvider>
     </>
   );
 };
